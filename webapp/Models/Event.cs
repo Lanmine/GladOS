7bf9f865bd2b60f3f22a1abe47d23c7b22f5c114
Athiene <arvinrapstad@gmail.com>
2022-12-03 16:32:36 +0100

    Created the Event class. It requires some minor details like having a limit for the different strings etc.

---
+++ webapp/Models/Event.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapp.Models;
+
+public class Event
+{
+    public Event() {}
+    
+    [Required]
+    public Guid Id { get; set; }
+
+    public bool? Archived { get; set; } = false;
+    
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    [Required] public string Description { get; set; } = string.Empty;
+
+
+    [Required]
+    public DateTime Start { get; set; }
+    
+    [Required]
+    public DateTime End { get; set; }
+    
+    [Required]
+    public DateTime DateCreated { get; set; }
+    
+    public DateTime? DateModified { get; set; }
+    
+    [Required]
+    public string HumanReadableUrl { get; set; } = string.Empty;
+}
