c3b3cf771f50db9100049c11da20bdba0b5c0eb4
Mathias René Juelsen-Jacobsen <54852665+Artx999@users.noreply.github.com>
2022-12-03 03:21:36 +0100

    Added first and last name to ApplicationUser
    Also scaffolded code for registration and added the new attributes as fields.
    Had to add package Microsoft.VisualStudio.Web.CodeGeneration.Design as it wasn't added before.

--- webapp/Models/ApplicationUser.cs
+++ webapp/Models/ApplicationUser.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace webapp.Models;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
+    [Required]
+    public string FirstName { get; set; }
     
+    [Required]
+    public string LastName { get; set; }
+    
+    [StringLength(64, MinimumLength = 4)]
+    public string? Nickname;
+    
+    //[Required]
+    //public DateOnly DateOfBirth;
+    
+    //public DateTime DateCreated { get; } = DateTime.Now;
+    //public DateTime DateModified { get; set; }
+
+    //public ExtraInfo ExtraInfo;
+
+    //public File ProfilePicture;
+    //public ExtraInfo m_ExtraInfo;
 }
